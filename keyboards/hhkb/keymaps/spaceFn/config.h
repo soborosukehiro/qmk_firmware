9fae6a1733a923b7af80ddee35418d5b3c391256
yututi <yututi@users.noreply.github.com>
2017-08-20 02:42:16 +0900

    add keymap

---
+++ keyboards/hhkb/keymaps/spaceFn/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_SPACEFN_H
+#define CONFIG_SPACEFN_H
+
+#include "../../config.h"
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 230
+
+#define TAPPING_TOGGLE 4
+
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 1000
+
+#define MOUSEKEY_INTERVAL 10
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 5
+
+#endif
